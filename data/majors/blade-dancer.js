13a2861113db68dd2f12cbf46ed8d3739ed40587
Pyriand <ptan02@gmail.com>
2022-04-07 13:58:51 +0300

    Update blade-dancer.js
    added domain 'battle'

--- data/majors/blade-dancer.js
+++ data/majors/blade-dancer.js
@@ -3,11 +3,11 @@ export default {
   id: 'blade-dancer',
   name: 'Blade Dancer',
   powers: ['slashing-grace', 'blood-price'],
-  requirements: ['domain:war'],
+  requirements: ['domain:war', 'domain:battle']
   stats: {
     'armor-penetration-slashing': 2,
     'critical-damage': 5
   },
   type: 'major',
-  version: '6.540'
+  version: '7.700'
 };
